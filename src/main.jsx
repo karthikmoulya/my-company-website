e282104719ed089c0cea43b6bc6ea1416afb9e79
karthikmoulya <karthikmoulya1995@gmail.com>
2025-04-06 09:16:11 +0530

    minor fix

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,14 @@
+// src/main.jsx or src/index.jsx
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom'; // 👈 updated
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename='/my-company-website'>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>
 );
